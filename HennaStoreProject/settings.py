57f912db0c1d5f1ce2d7e4aa4b252c1545bd5763
rahmafaris981 <rahmafaris981@gmail.com>
2024-08-12 16:08:23 +0300

    First Project

--- HennaStoreProject/settings.py
+++ HennaStoreProject/settings.py
@@ -152,3 +152,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # or any other directory you prefer
